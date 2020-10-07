ae180ff3102e223fbae86b97a6ebbaa854301b8b
Stefanos Mandalas <sealmove@protonmail.com>
2020-10-07 17:48:11 +0300

    misc formatting changes

--- src/o.nim
+++ src/o.nim
@@ -43,10 +43,11 @@ proc findGroup(arg: string): string =
   case matches.len
   of 0:
     error("Group not found")
+    quit QuitFailure
   of 1:
     result = matches[0]
   else:
-    error("Ambiguous group; multiple matches:")
+    error("Ambiguous group match:")
     for m in matches:
       echo "  " & colored(m, fgYellow)
     quit QuitFailure
@@ -64,25 +65,27 @@ proc findAlias(group, arg: string): tuple[alias, cmd: string] =
     for k, v in matches.pairs:
       result = (k, v)
   else:
-    error("Ambiguous alias; multiple matches:")
+    error("Ambiguous alias match:")
     for alias, cmds in matches.pairs:
       echo "  " & colored(alias, fgYellow)
     quit QuitFailure
 
 proc printGroup(group: string) =
-  echo "\n" & styled("[" & group & "]", styleBright, fgYellow)
   for alias, cmd in cfg[group]:
-    echo fmt" {alias:<10}" & styled("| ", styleBright, fgYellow) & pretty(cmd)
+    echo fmt"  {alias:<10}" & styled("| ", styleBright, fgYellow) & pretty(cmd)
 
 if paramCount() == 0:
   echo "Available groups:"
   for g in cfg.keys:
     echo "  " & styled(g, styleBright, fgYellow)
-  quit QuitFailure
+  quit QuitSuccess
+
 let g = findGroup(paramStr(1))
+
 if paramCount() == 1:
   printGroup(g)
   quit QuitSuccess
+
 var (alias, cmd) = findAlias(g, paramStr(2))
 
 # Check if arguments match parameters
